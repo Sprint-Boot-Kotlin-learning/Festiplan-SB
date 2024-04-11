eb95d7a09745872b425c4945687df15c592d8602
François SP <francois-sp@gmx.fr>
2024-04-11 11:22:07 +0200

    Initial commit

---
+++ src/main/kotlin/com/fsp/festiplansb/FestiplanSbApplication.kt
@@ -0,0 +1,11 @@
+package com.fsp.festiplansb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FestiplanSbApplication
+
+fun main(args: Array<String>) {
+    runApplication<FestiplanSbApplication>(*args)
+}
---
+++ src/test/kotlin/com/fsp/festiplansb/FestiplanSbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fsp.festiplansb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FestiplanSbApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
